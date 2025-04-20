0c727ab30330380fabd8ec776b335fb2e6fa9041
Jexzas <jdrmail2007@yahoo.com>
2025-04-20 16:52:28 -0500

    try to fix the sql query

--- obj/Debug/net9.0-windows/clientScheduler.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/clientScheduler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clientScheduler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b7343133de20846beb8e5c7b45cc63c950b1bfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48a4c2f7fafeceb977427b6df90de0ade47c5b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("clientScheduler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clientScheduler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
